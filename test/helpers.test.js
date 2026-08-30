f3f67d6199a24443b6c3ed92ea2c2cb599b0c5c3
yangqinzhenzi945 <yangqinzhenzi@gmail.com>
2026-08-30 12:21:36 +0200

    Fix UTF-8 encoding in test/helpers.test.js

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -2,17 +2,17 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { normalizePostalCode, outputPathFor, parseShipmentResult } from "../src/helpers.js";
 
-test("寰峰浗鍥涗綅閭紪鑷姩琛ラ浂", () => {
+test("德国四位邮编自动补零", () => {
   assert.equal(normalizePostalCode(9212), "09212");
   assert.equal(normalizePostalCode("07745"), "07745");
 });
 
-test("瑙ｆ瀽 DHL 缁撴灉椤?, () => {
+test("解析 DHL 结果页", () => {
   const result = parseShipmentResult("Sendungsnummer: 123456789012\nKundenreferenz: test-ref-001\n");
   assert.deepEqual(result, { shipmentNumber: "123456789012", customerReference: "test-ref-001" });
 });
 
-test("榛樿杈撳嚭鏂囦欢涓嶈鐩栨簮鏂囦欢", () => {
-  assert.match(outputPathFor("C:/data/DHL閫€璐ф爣绛?xlsx"), /DHL閫€璐ф爣绛綺宸插～鍐欒繍鍗曞彿\.xlsx$/);
+test("默认输出文件不覆盖源文件", () => {
+  assert.match(outputPathFor("C:/data/DHL退货标签.xlsx"), /DHL退货标签_已填写运单号\.xlsx$/);
 });
 
